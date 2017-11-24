c9cd71eb2fc4dc27f88c8226eb5f4b9e0f3bcef6
Filip <antic.filip996@gmail.com>
2017-11-24 22:32:50 +0100

    Napravljen je ceo log izgled

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.createForm();
   }
 
+  createForm() {
+    this.loginForm = this.formBuilder.group({
+      'username': [null],
+      'password': [null],
+    })
+  }
+
+  save() {
+
+  }
 }
